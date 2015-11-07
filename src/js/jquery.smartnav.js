fa1a4aa5802f7d3033b7724c5fe360ea96e47e54
David Moothen <dmoothen@websitti.fr>
2015-11-07 18:14:06 +0100

    Auto update navigation items when scrolling the page.

--- src/js/jquery.smartnav.js
+++ src/js/jquery.smartnav.js
@@ -1,18 +1,44 @@
-(function($){
+(function ($) {
 
-    $.fn.smartnav = function(params){
-        
+    $.fn.smartnav = function (params) {
+
         var defaults = {
             duration: 1000
         };
-                
+
         params = $.extend({}, defaults, params);
-        
-        return this.each(function(element){
-            $menu = $(this);
-            $('a', $menu).click(function(e){
+
+        return this.each(function (element) {
+            var $menu = $(this);
+            var $links = $('a', $menu);
+            var $window = $(window);
+            var sections = [];
+            var id = null;
+            var scrollId = null;
+            $links.each(function (i, link) {
+                sections.push($(link).attr('href'));
+            });
+
+            $window.scroll(function (e) {
+                var scrollTop = $window.scrollTop();
+                var $s = null;
+                for (var i in sections) {
+                    $s = $(sections[i]);
+                    if (scrollTop > $s.offset().top - $window.height() / 2)
+                        scrollId = sections[i];
+                }
+
+                if (id !== scrollId) {
+                    id = scrollId;
+                    $links.removeClass('selected');
+                    $('a[href=' + id + ']', $menu).addClass('selected');
+                }
+
+            });
+
+            $links.click(function (e) {
                 e.preventDefault();
-                
+
                 var to = 0;
                 var id = $(this).attr('href');
 
@@ -20,15 +46,15 @@
                 // If id isn't an anchor then stop
                 if (!id || id.charAt(0) !== '#')
                     return false;
-                
+
                 // id is an anchor, if id is found then go to top offset
                 if (id !== '#')
                     to = $(id).offset().top;
-                
+
                 $('html,body').animate({
                     scrollTop: to
                 }, params.duration);
-                
+
                 // Update browse url and history
                 if (history.pushState) {
                     history.pushState(null, null, id);
@@ -38,5 +64,5 @@
             });
         });
     };
-    
+
 })(jQuery);
